c70c15d66c85d20a40ea526bdd738b4522d1433e
cnoble25 <cnoble25@students.stab.org>
2024-03-26 10:58:44 -0400

    stuff

--- search.js
+++ search.js
@@ -238,16 +238,3 @@ function displaySearchHistory() {
 }
 //run once to make sure working
 displayStudentSearchData();
-function hash(string) {
-    const utf8 = new TextEncoder().encode(string);
-    return crypto.subtle.digest('SHA-256', utf8).then((hashBuffer) => {
-      const hashArray = Array.from(new Uint8Array(hashBuffer));
-      const hashHex = hashArray
-        .map((bytes) => bytes.toString(16).padStart(2, '0'))
-        .join('');
-      return hashHex;
-    });
-  }
-
-hash("stabls").then((hex) => console.log(hex));
-
